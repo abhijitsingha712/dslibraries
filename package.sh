42c6a1a2a6a20eaf47e8397ba5e60161b44e8e73
abhijitsingha712 <41609886+abhijitsingha712@users.noreply.github.com>
2019-11-13 15:45:51 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo /usr/bin/anaconda/envs/py35/bin/python3 -m pip install --upgrade pip
-sudo /usr/bin/anaconda/envs/py35/bin/python3 -m pip install numpy==1.15.4
-sudo /usr/bin/anaconda/envs/py35/bin/python3 -m pip install pyarrow==0.14.1
-sudo /usr/bin/anaconda/envs/py35/bin/python3 -m pip install pandas==0.25.1
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install numpy==1.16.0
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install pyarrow==0.14.1
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install pandas==0.23.4
 sudo /usr/bin/anaconda/envs/py35/bin/python3 -m pip install ts-flint
