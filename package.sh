6a2788c0efbab52869ed10b81bfa606480fe1c4b
Abhijit Singha <abhijit.singha@ip-172-31-102-55.ec2.internal>
2019-11-13 15:28:48 +0530

    first commit

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install --upgrade pip
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install numpy==1.15.4
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install pyarrow==0.14.1
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install pandas==0.25.1
+/usr/bin/anaconda/envs/py35/bin/python3 -m pip install ts-flint
